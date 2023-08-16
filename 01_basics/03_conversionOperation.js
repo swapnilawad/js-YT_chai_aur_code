671524347d38049d7ec39a6fe71d422c90d13ec9
Swapnil_Awad <swapnilawad1413@gmail.com>
2023-08-16 17:42:55 +0530

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,55 @@
+let score = "33";
+// let score = "33abc";  
+// let score = null;
+// let score = true;
+// let score = "hitesh";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number (score);
+
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+let isLoggedIn =1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// *********************** 
+
+// 1 >> true 
+// 0 >> flase
+//  "swap" >> true 
+
+let someNumber = 33;
+let stringnumber = String (someNumber);
+console.log(stringnumber);
+console.log(typeof(stringnumber));
+
+console.log("******************************************")
+// ****************** operations ********************
+
+let vlaue = 3;
+let negvalue = -vlaue;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1= "hello";
+let str2= " swapnil";
+
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
