2564f8a36e43441a30760fd6659ffd395e364aef
Swapnil_Awad <swapnilawad1413@gmail.com>
2023-08-16 16:59:24 +0530

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accoutnID=123456;
+let accountEmail="swap@google.com";
+var accoutnPassword="789456";
+accoutnCity = "Aurangabad";
+
+// accountId = 2 // not allowed
+
+accountEmail="sa@goole.com"; 
+accoutnPassword="123456789";
+accoutnCity="Pune";
+console.log(accountEmail);
+
+
+ 
+console.table([accountEmail,accoutnID,accoutnPassword]);
