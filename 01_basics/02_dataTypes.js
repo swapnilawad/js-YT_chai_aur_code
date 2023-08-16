530566fe98cf9b9bcd6b3275fcd50de436f7d787
Swapnil_Awad <swapnilawad1413@gmail.com>
2023-08-16 17:41:45 +0530

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+let name  = "swapnil";
+let age = 18;
+let isLoggedIn = false;
+
+// number >> 2 to power 53
+// bigint
+// string >> ""
+// boolean >> true / false
+// null  >> stnad alone value
+// undefined >> 
+// symbol 
+
+// *************************
+// object 
+
+console.log(typeof"swap");
